8f7e30801384c485de235d86d720efdeb91d6bdb
ZhangGJ <ZhangGJFJ@163.com>
2020-02-16 06:50:38 +0800

    ZhangGJ:On java 8:Everything is objects

--- Chapter03/src/main/java/section_04/Assignment.java
+++ Chapter03/src/main/java/section_04/Assignment.java
@@ -8,6 +8,7 @@ class Tank {
 
 
 /**
+ * On Java 8
  * important
  * 对象赋值
  * @Author ZhangGJ
@@ -20,6 +21,11 @@ public class Assignment {
         t1.level = 9;
         t2.level = 47;
         print("1: t1.level: " + t1.level + ", t2.level: " + t2.level);
+        /**
+         * 由于赋予的只是对象的引用，改变 t1 也就改变了 t2。 这是因为 t1 和 t2 此时指向的是堆中同一个对象。（t1
+         * 原始对象的引用在 t2 赋值给其时丢失，它引用的对象会在垃圾回收时被清理）
+         * 这种现象通常称为别名（aliasing）
+         */
         t1 = t2;
         print("2: t1.level: " + t1.level + ", t2.level: " + t2.level);
         /**
