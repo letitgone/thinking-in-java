aea2df0c4d0405dff9ecaaed0fb2080385c40378
ZhangGJ <48301286+letitgone@users.noreply.github.com>
2020-08-01 23:00:27 +0800

    ZhangGJ:Optimize the code

--- Chapter12/src/main/java/section_06/DynamicFields.java
+++ Chapter12/src/main/java/section_06/DynamicFields.java
@@ -15,10 +15,12 @@ public class DynamicFields {
 
     public DynamicFields(int initialSize) {
         fields = new Object[initialSize][2];
-        for (int i = 0; i < initialSize; i++)
+        for (int i = 0; i < initialSize; i++) {
             fields[i] = new Object[] {null, null};
+        }
     }
 
+    @Override
     public String toString() {
         StringBuilder result = new StringBuilder();
         for (Object[] obj : fields) {
@@ -31,31 +33,37 @@ public class DynamicFields {
     }
 
     private int hasField(String id) {
-        for (int i = 0; i < fields.length; i++)
-            if (id.equals(fields[i][0]))
+        for (int i = 0; i < fields.length; i++) {
+            if (id.equals(fields[i][0])) {
                 return i;
+            }
+        }
         return -1;
     }
 
     private int getFieldNumber(String id) throws NoSuchFieldException {
         int fieldNum = hasField(id);
-        if (fieldNum == -1)
+        if (fieldNum == -1) {
             throw new NoSuchFieldException();
+        }
         return fieldNum;
     }
 
     private int makeField(String id) {
-        for (int i = 0; i < fields.length; i++)
+        for (int i = 0; i < fields.length; i++) {
             if (fields[i][0] == null) {
                 fields[i][0] = id;
                 return i;
             }
+        }
         // No empty fields. Add one:
         Object[][] tmp = new Object[fields.length + 1][2];
-        for (int i = 0; i < fields.length; i++)
+        for (int i = 0; i < fields.length; i++) {
             tmp[i] = fields[i];
-        for (int i = fields.length; i < tmp.length; i++)
+        }
+        for (int i = fields.length; i < tmp.length; i++) {
             tmp[i] = new Object[] {null, null};
+        }
         fields = tmp;
         // Recursive call with expanded fields:
         return makeField(id);
@@ -75,8 +83,9 @@ public class DynamicFields {
             throw dfe;
         }
         int fieldNumber = hasField(id);
-        if (fieldNumber == -1)
+        if (fieldNumber == -1) {
             fieldNumber = makeField(id);
+        }
         Object result = null;
         try {
             result = getField(id); // Get old value
