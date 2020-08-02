c8064e2d5ac14832d63d404909299334f59ee26a
ZhangGJ <48301286+letitgone@users.noreply.github.com>
2020-08-02 10:32:19 +0800

    ZhangGJ:Optimize the code

--- Chapter13/src/main/java/section_02/UsingStringBuilder.java
+++ Chapter13/src/main/java/section_02/UsingStringBuilder.java
@@ -9,6 +9,7 @@ import java.util.Random;
 public class UsingStringBuilder {
     public static Random rand = new Random(47);
 
+    @Override
     public String toString() {
         StringBuilder result = new StringBuilder("[");
         for (int i = 0; i < 25; i++) {
